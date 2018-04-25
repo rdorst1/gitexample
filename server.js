4676a4b1299e8e67060b7f1a3031cdc0cb737b4b
Rowan Dorst <rdorst1@avans.nl>
2018-04-25 14:06:21 +0200

    alles

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get('/hello', (req, res) => {
     res.json({'msg': 'Hello You All'})
 });
 
+const port = process.env.PORT || 8080;
+
 app.listen(8080, () => {
     console.log('server is ready')
 });
